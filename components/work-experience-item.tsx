2710f250c277b15d2d9d16a905452eb64f80feda
rbpolim <rbpolim@gmail.com>
2024-01-04 20:26:05 -0300

    feat: work experience item component

---
+++ components/work-experience-item.tsx
@@ -0,0 +1,29 @@
+import { Badge } from "@/components/ui/badge";
+import { WorkExperience } from "@/data/resume-work-experience";
+
+type WorkExperienceItemProps = {
+  data: WorkExperience;
+};
+
+export const WorkExperienceItem = ({ data }: WorkExperienceItemProps) => {
+  return (
+    <div className="space-y-1">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <h3 className="text-lg font-semibold">{data.company}</h3>
+          {data.isRemote && <Badge>Remote</Badge>}
+        </div>
+        <span className="text-muted-foreground">
+          {data.startYear} - {data.endYear}
+        </span>
+      </div>
+      <h4 className="text-muted-foreground font-semibold">{data.position}</h4>
+      <p className="text-sm text-muted-foreground tracking-wide leading-normal">
+        {data.description}
+      </p>
+      <p className="text-sm font-medium">
+        Tags: {data.technologies}
+      </p>
+    </div>
+  );
+};
