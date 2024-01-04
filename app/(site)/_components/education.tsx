62227dce4832303b1e7becc804904a17a0f715fe
rbpolim <rbpolim@gmail.com>
2024-01-04 20:25:18 -0300

    feat: education component

---
+++ app/(site)/_components/education.tsx
@@ -0,0 +1,16 @@
+export const Education = () => {
+  return (
+    <section className="space-y-2">
+      <h2 className="text-2xl font-bold">Education</h2>
+      <div className="flex items-start justify-between">
+        <div>
+          <h3 className="text-lg font-semibold">Senac</h3>
+          <p className="text-sm text-pretty text-muted-foreground tracking-wide leading-normal">
+            Bachelor's Degree in Control systems engineering and Robotics
+          </p>
+        </div>
+        <p className="text-muted-foreground">2022 - 2024</p>
+      </div>
+    </section>
+  );
+};
